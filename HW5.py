e892fe2896b254eec68ab133b50621f70f70aa6c
unknown <Student@DHG274Q1.virginia.edu>
2014-12-01 15:48:53 -0500

    From sklearn.cross_validation use the train_test_split method to split the data and corresponding labels into a training set and a testing set.

--- HW5.py
+++ HW5.py
@@ -7,6 +7,8 @@ Created on Sun Nov 30 21:29:39 2014
 
 import pandas
 import numpy as np
+from sklearn import cross_validation
+
 
 wine = pandas.read_csv("wine.data", header=None)
 wine.columns = ['Class','Alcohol','Malic Acid','Ash','Alcalinity of Ash','Magnesium','Total Phenols','Flavanoids','Nonflavanoid Phenols','Proanthocyanins','Color Intensity','Hue','OD280/OD315 of Diluted Wines','Proline']
@@ -18,3 +20,6 @@ all_array=np.array(data_values)
 data_array=all_array[:,1:]
 label_array=all_array[:,0:1]
 label_array=label_array.ravel()
+
+
+data_train, data_test, label_train, label_test = cross_validation.train_test_split(data_array,label_array)
