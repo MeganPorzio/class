29d575a440ab99f55691e0f223b3d422df27910b
unknown <Student@DHG274Q1.virginia.edu>
2014-11-30 22:42:53 -0500

    Columns and Alcohol Histogram

--- HW5.py
+++ HW5.py
@@ -4,5 +4,13 @@ Created on Sun Nov 30 21:29:39 2014
 
 @author: Student
 """
-import os
-os.getcwd()
+
+import pandas
+import matplotlib.pyplot as plt
+
+wine = pandas.read_csv("wine.data", header=None)
+wine.columns = ['Class','Alcohol','Malic Acid','Ash','Alcalinity of Ash','Magnesium','Total Phenols','Flavanoids','Nonflavanoid Phenols','Proanthocyanins','Color Intensity','Hue','OD280/OD315 of Diluted Wines','Proline']
+
+print wine
+
+wine.hist([1])
