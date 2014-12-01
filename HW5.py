11d7f7dc5204f48e7a46e2249c0d30744754b98b
unknown <Student@DHG274Q1.virginia.edu>
2014-12-01 15:04:26 -0500

    Data into arrays

--- HW5.py
+++ HW5.py
@@ -6,11 +6,15 @@ Created on Sun Nov 30 21:29:39 2014
 """
 
 import pandas
-import matplotlib.pyplot as plt
+import numpy as np
 
 wine = pandas.read_csv("wine.data", header=None)
 wine.columns = ['Class','Alcohol','Malic Acid','Ash','Alcalinity of Ash','Magnesium','Total Phenols','Flavanoids','Nonflavanoid Phenols','Proanthocyanins','Color Intensity','Hue','OD280/OD315 of Diluted Wines','Proline']
 
-print wine
-
 wine.hist([1])
+
+data_values=wine.values
+all_array=np.array(data_values)
+data_array=all_array[:,1:]
+label_array=all_array[:,0:1]
+label_array=label_array.ravel()
