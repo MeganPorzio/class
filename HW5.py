476e53f0c2c9f24ba450bb34c8adeccded49507d
unknown <Student@DHG274Q1.virginia.edu>
2014-12-02 21:50:42 -0500

    knn

--- HW5.py
+++ HW5.py
@@ -9,7 +9,7 @@ import pandas
 import numpy as np
 from sklearn import cross_validation
 from sklearn.linear_model import LogisticRegression
-
+from sklearn.neighbors import KNeighborsClassifier
 
 wine = pandas.read_csv("wine.data", header=None)
 wine.columns = ['Class','Alcohol','Malic Acid','Ash','Alcalinity of Ash','Magnesium','Total Phenols','Flavanoids','Nonflavanoid Phenols','Proanthocyanins','Color Intensity','Hue','OD280/OD315 of Diluted Wines','Proline']
@@ -25,16 +25,25 @@ label_array=label_array.ravel()
 
 data_train, data_test, label_train, label_test = cross_validation.train_test_split(data_array,label_array)
 
-regression = LogisticRegression().fit(data_train,label_train)
-prediction = regression.predict(data_test)
-label_test
+lr = LogisticRegression().fit(data_train,label_train)
+lr_prediction = lr.predict(data_test)
 
 i = 0
 count = 0
 while i <= 44:
-    if prediction[i] == label_test[i]:
+    if lr_prediction[i] == label_test[i]:
         count += 1
     i+=1
 
 print "Number of correct predictions: "
 print count
+
+knn = KNeighborsClassifier().fit(data_train,label_train)
+knn_prediction = knn.predict(data_test)
+
+ki = 0
+kcount = 0
+while ki <= 44:
+    if knn_prediction[ki] == label_test[ki]:
+        count += 1
+    i+=1
